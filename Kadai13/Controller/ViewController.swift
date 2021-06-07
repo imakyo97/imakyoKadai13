d94847b0e587543b313f841e4df420b7a743fd49
akio0911 <akio0911@gmail.com>
2021-06-08 06:44:44 +0900

    feedback

--- Kadai13/Controller/ViewController.swift
+++ Kadai13/Controller/ViewController.swift
@@ -7,34 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDataSource {
-    
-    private let itemCell = ItemCell()
+class ViewController: UIViewController, UITableViewDataSource {
+
+    private let checkItems: [CheckItem] = [
+        CheckItem(name: "りんご", isChecked: false),
+        CheckItem(name: "みかん", isChecked: true),
+        CheckItem(name: "バナナ", isChecked: false),
+        CheckItem(name: "パイナップル", isChecked: true)
+    ]
+
     @IBOutlet private weak var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
     }
-    
+
     // MARK: - UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemCell.Items.count
+        checkItems.count
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // swiftlint:disable:next force_cast
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! ItemTableViewCell
-        cell.fruitsLabel.text = itemCell.Items[indexPath.row][itemCell.fruitsKey] as? String
-        cell.cheakImage.image = nil
-        if itemCell.Items[indexPath.row][itemCell.imageKey] as! Bool {
-            cell.cheakImage.image = UIImage(named: "CheakMark")
-        }
+        cell.configure(checkItem: checkItems[indexPath.row])
         return cell
     }
-    
 }
-
---
+++ Kadai13/Model/CheckItem.swift
@@ -0,0 +1,13 @@
+//
+//  CheckItem.swift
+//  Kadai13
+//
+//  Created by 今村京平 on 2021/06/06.
+//
+
+import Foundation
+
+struct CheckItem {
+    var name: String
+    var isChecked: Bool
+}
